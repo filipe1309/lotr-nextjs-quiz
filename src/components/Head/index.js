2b34a807cd937d780e2155472fd2fe1f43fd9037
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-26 13:09:09 -0300

    feat: add Lato Google font into head

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -20,6 +20,8 @@ function Head({ title, image, description }) {
         <meta charSet="utf-8" />
 
         <link rel="canonical" href="https://lotr-quiz.filipe1309.vercel.app/" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
     </NextHead>
   )
 }
