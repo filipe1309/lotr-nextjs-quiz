9daea2428cfd7fe96e32f83a77c938caecb8efa6
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-27 01:00:32 -0300

    reafactor: improve quiz page

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,10 +1,33 @@
-import { func } from 'prop-types';
 import React from 'react';
+import db from '../db.json';
+import QuizBackground from '../components/QuizBackground';
+import QuizContainer from '../components/QuizContainer';
+import Widget from '../components/Widget';
+import { useRouter } from 'next/router'
 
 export default function QuizPage() {
+  // const urlParams = new URLSearchParams(window.location.search);
+  // const name = urlParams.get('name');
+  const router = useRouter()
+  const { name } = router.query
+  
   return (
-    <div>
-      Página do Quiz
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
+        <Widget>
+          <Widget.Header>
+            <h1>Página do Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            Bem vind@ <strong>{name}</strong>!!!
+            <br/>
+            <br/>
+            <img src={db.questions[0].image} alt={db.questions[0].title}/>
+          </Widget.Content>
+        </Widget>
+
+      </QuizContainer>
+    </QuizBackground>
   );
 };
