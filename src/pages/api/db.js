51b3ea0186df0816fa8216a99c5bfd1066e24a26
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-31 22:13:22 -0300

    fix: add check to avoid wrong db remap concat

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -12,15 +12,17 @@ export default function dbHandler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
 
   // Remap local images, to allow ohers to use
-  const { host } = req.headers;
-  const protocol = req.protocol ? (`${req.protocol}://`) : 'http://';
-  db.bg = protocol + host + db.bg;
+  if (!db.bg.includes('http')) {
+    const { host } = req.headers;
+    const protocol = req.protocol ? (`${req.protocol}://`) : 'http://';
+    db.bg = protocol + host + db.bg;
 
-  db.questions.map((question) => {
-    // eslint-disable-next-line no-param-reassign
-    question.image = protocol + host + question.image;
-    return question;
-  });
+    db.questions.map((question) => {
+      // eslint-disable-next-line no-param-reassign
+      question.image = protocol + host + question.image;
+      return question;
+    });
+  }
 
   res.json(db);
 }
